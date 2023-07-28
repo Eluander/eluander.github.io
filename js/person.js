eb61ad500173c784a05af5368a208c28806f822c
Eluander J. F. Lopes <eluander@gmail.com>
2023-07-27 23:04:25 -0300

    Add files via upload

---
+++ js/person.js
@@ -0,0 +1,307 @@
+{
+   "nomeCompleto": "Eluander Josué Frigo Lopes",
+   "nomeAbreviado": "Eluander J. F. Lopes",
+   "apelido": "Eluander",
+   "titulo": "Desenvolvedor de Sistemas",
+   "dataNascimento": "1994-01-18",
+   "naturalidade": "Campo Grande - MS",
+   "conjuge": "Fernanda da Silva",
+   "sexo": "Masculino",
+   "sobre": {
+      "descricao": "Seja bem vindo a minha página, meu nome é Eluander Josué Frigo Lopes, nascido na cidade de Campo Grande - MS. Após concluir o ensino médio, mudei para o estado do Paraná, onde passei por algumas cidades, até conhecer minha esposa Fernanda da Silva e decidimos morar juntos em Coronel Vivida - PR. Me formei no ano de 2015 no curso de Análise e Desenvolvimento de Sistemas, mas sempre digo que meu maior aprendizado foi adquirido com muitas pesquisas, cursos e principalmente na prática dentro das empresas que trabalhei. Trabalho com desenvolvimento de sistemas web desde o final de 2013 e até hoje não parei mais. Sou curioso e gosto de estar aprendendo coisas novas, com muita determinação busco estar evoluindo continuamente tanto meu lado pessoal, quanto meu lado profissional. Não me contento com pouco, quero sempre mais!"
+   },
+   "contato": [
+		{
+			 "tipo": "celular",
+			 "data": "(46)999302905"
+		},
+		{
+			 "tipo": "email",
+			 "data": "eluander@gmail.com"
+		},
+		{
+			 "tipo": "website",
+			 "data": "eluander.com.br"
+		},
+		{
+			 "tipo": "whatsapp",
+			 "data": "https://api.whatsapp.com/send?phone=5546999302905&text=Continue%20para%20entrar%20em%20contato%20com%20Eluander."
+		},
+		{
+			 "tipo": "linkedin",
+			 "data": "https://www.linkedin.com/in/eluander"
+		},
+		{
+			 "tipo": "instagran",
+			 "data": "https://www.instagram.com/eluanderj"
+		},
+		{
+			 "tipo": "facebook",
+			 "data": "https://www.facebook.com/eluander"
+		},
+		{
+			 "tipo": "github",
+			 "data": "https://github.com/Eluander"
+		},
+		{
+			 "tipo": "youtube",
+			 "data": "https://www.youtube.com/channel/UCBVFOT9sP1DQ35jrUa_CCkA"
+		}
+   ],
+   "endereco": [
+		{
+			 "rua": "Rua Denir Framento",
+			 "numero": "39",
+			 "bairro": "Lisboa da Silva",
+			 "cep": "85550-000",
+			 "complemento": "Casa",
+			 "cidade": "Coronel Vivida",
+			 "estado": "Paraná",
+			 "sigla": "PR"
+		}
+   ],
+   "educacao": [
+		{
+			 "instituicao": "FADEP Faculdade de Pato Branco",
+			 "curso": "Tecnologia em Análise e Desenvolvimento de Sistemas",
+			 "nivel": "Graduação",
+			 "situacao": "Completo",
+			 "dataInicio": "2013-01-01",
+			 "dataFim": "2015-06-01"
+		},
+		{
+			 "instituicao": "Escola Estadual Padre José Scampini",
+			 "curso": "Ensino Médio",
+			 "nivel": "Escolar",
+			 "situacao": "Completo",
+			 "dataInicio": null,
+			 "dataFim": null
+		},
+		{
+			 "instituicao": "Escola Estadual Delmira Ramos dos Santos",
+			 "curso": "Ensino Fundamental",
+			 "nivel": "Escolar",
+			 "situacao": "Completo",
+			 "dataInicio": null,
+			 "dataFim": null
+		}
+   ],
+   "profissional": [
+		{
+			 "empresa": "Firjan",
+			 "funcao": "Programador"
+		},
+		{
+			 "empresa": "Pulsati/Movidesk",
+			 "funcao": "Programador"
+		},
+		{
+			 "empresa": "Supera Sistemas",
+			 "funcao": "Programador"
+		},
+		{
+			 "empresa": "GNC - Sistemas para Medicina do Trabalho",
+			 "funcao": "Programador"
+		},
+		{
+			 "empresa": "Yupi Restaurante e Pizzaria",
+			 "funcao": "Operador de Caixa"
+		},
+		{
+			 "empresa": "INCRA MS - Instituto Nacional Colonizador de Reforma Agrária",
+			 "funcao": "Administração de Transportes"
+		}
+   ],
+   "habilidade": [
+		{
+			 "feature": "Vontade de Aprender Uma Nova Feature",
+			 "nivel": 100,
+			 "tipo": "Generico"
+		},
+		{
+			 "feature": "C#",
+			 "nivel": 95,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "Javascript",
+			 "nivel": 95,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "HTML5",
+			 "nivel": 90,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "CSS3",
+			 "nivel": 85,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "JQuery",
+			 "nivel": 90,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "Razor",
+			 "nivel": 90,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "Java",
+			 "nivel": 60,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "VB6",
+			 "nivel": 50,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "VB.NET",
+			 "nivel": 60,
+			 "tipo": "Linguagem"
+		},
+		{
+			 "feature": "Angular",
+			 "nivel": 70,
+			 "tipo": "Design System"
+		},
+		{
+			 "feature": "React JS",
+			 "nivel": 50,
+			 "tipo": "Design System"
+		},
+		{
+			 "feature": "Bootstrap",
+			 "nivel": 95,
+			 "tipo": "Design System"
+		},
+		{
+			 "feature": "Material Design",
+			 "nivel": 70,
+			 "tipo": "Design System"
+		},
+		{
+			 "feature": "Ionic",
+			 "nivel": 50,
+			 "tipo": "Design System"
+		},
+		{
+			 "feature": "Primefaces",
+			 "nivel": 50,
+			 "tipo": "Design System"
+		},
+		{
+			 "feature": "EXT JS",
+			 "nivel": 60,
+			 "tipo": "Design System"
+		},
+		{
+			 "feature": "SQL Server",
+			 "nivel": 70,
+			 "tipo": "Banco de Dados"
+		},
+		{
+			 "feature": "PostgreSQL",
+			 "nivel": 75,
+			 "tipo": "Banco de Dados"
+		},
+		{
+			 "feature": "NHibernate",
+			 "nivel": 85,
+			 "tipo": "ORM"
+		},
+		{
+			 "feature": "Entity Framework",
+			 "nivel": 85,
+			 "tipo": "ORM"
+		},
+		{
+			 "feature": "Azure",
+			 "nivel": 70,
+			 "tipo": "Plataforma"
+		},
+		{
+			 "feature": "AWS",
+			 "nivel": 70,
+			 "tipo": "Plataforma"
+		},
+		{
+			 "feature": "GITHUB",
+			 "nivel": 70,
+			 "tipo": "Controlador de Código Fonte"
+		},
+		{
+			 "feature": "Bitbucket",
+			 "nivel": 70,
+			 "tipo": "Controlador de Código Fonte"
+		},
+		{
+			 "feature": "GitLab",
+			 "nivel": 70,
+			 "tipo": "Controlador de Código Fonte"
+		},
+		{
+			 "feature": "Visual Studio",
+			 "nivel": 90,
+			 "tipo": "IDE"
+		},
+		{
+			 "feature": "Visual Studio Code",
+			 "nivel": 75,
+			 "tipo": "IDE"
+		},
+		{
+			 "feature": "Eclipse",
+			 "nivel": 60,
+			 "tipo": "IDE"
+		},
+		{
+			 "feature": "Android Studio",
+			 "nivel": 50,
+			 "tipo": "IDE"
+		},
+		{
+			 "feature": "SketchUP",
+			 "nivel": 75,
+			 "tipo": "Software"
+		},
+		{
+			 "feature": "Photoshop",
+			 "nivel": 85,
+			 "tipo": "Software"
+		},
+		{
+			 "feature": "AutoCAD",
+			 "nivel": ,
+			 "tipo": "Software"
+		},
+		{
+			 "feature": "IIS",
+			 "nivel": 80,
+			 "tipo" : "Servidor"
+		},
+		{
+			 "feature": "TFS",
+			 "nivel": 45,
+			 "tipo" : "Servidor"
+		},
+		{
+			 "feature": "DDD - Domain Driven Design",
+			 "nivel": 90,
+			 "tipo" : "Arquitetura de Software"
+		},
+		{
+			 "feature": "MVC - Model View Controller",
+			 "nivel": 90,
+			 "tipo" : "Arquitetura de Software"
+		},
+		{
+			 "feature": "TDD - Test Driven Development",
+			 "nivel": 60,
+			 "tipo" : "Arquitetura de Software"
+		}
+   ]
+}
